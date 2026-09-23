07d857e8379a29f9be4a37db0d3a6487a3e19aeb
unknown <oleg@yakovchenko.ru>
2026-09-24 00:20:29 +0300

    Add E402 to flake8 ignore

--- opinions_app/__init__.py
+++ opinions_app/__init__.py
@@ -8,13 +8,11 @@ app.config.from_object('settings.Config')
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-# Импорты из модулей пакета размещены в конце файла,
-# чтобы избежать циклических зависимостей:
-# views/models/cli_commands/error_handlers импортируют db,
-# а db должен быть создан раньше.
-from .views import blueprint                                     # noqa: E402
-from .cli_commands import load_opinions_command                  # noqa: E402
-from .error_handlers import internal_error, page_not_found       # noqa: E402
+# Импорты размещены после создания db,
+# чтобы избежать циклических зависимостей.
+from .views import blueprint                                # noqa: E402
+from .cli_commands import load_opinions_command             # noqa: E402
+from .error_handlers import internal_error, page_not_found  # noqa: E402
 
 app.register_blueprint(blueprint)
 app.cli.add_command(load_opinions_command)
